936376e5c9ac4b655ebb5679095f369b5a868ee5
Philipp Meschenmoser <Philipp.Meschenmoser@uni.kn>
2017-06-28 15:39:18 +0200

    image split

--- src/ColorHistogram.java
+++ src/ColorHistogram.java
@@ -7,15 +7,23 @@ import java.io.IOException;
  * Created by user on 28.06.2017.
  */
 public class ColorHistogram {
-    private BufferedImage buffered;
-    private int[] bins; // bins[0] = [#r-bins,#g-bins, #b-bins]
-    private int cells;
+    private BufferedImage source;
+    private BufferedImage cells[];
+    private int[] bincounts; // bincounts [#r-bins,#g-bins, #b-bins]
+    private int cellcount; //count per x resp. y dimension
     private int[][][] lookup;
 
-    public ColorHistogram(int[] bins, int cells) {
-        this.bins = bins;
-        this.cells = cells;
-        lookup = new int[bins[0]][bins[1]][bins[2]];
+    public ColorHistogram(int[] bincounts, int cellcount) {
+        this.bincounts = bincounts;
+        this.cellcount = cellcount;
+        lookup = new int[bincounts[0]][bincounts[1]][bincounts[2]];
+        empty();
+    }
+
+    public void empty(){
+        /*
+            Reset all color counts to 0.
+         */
         for (int r= 0; r<lookup.length; r++){
             for (int g = 0; g<lookup[r].length; g++){
                 for (int b = 0; b<lookup[r][g][b]; b++){
@@ -25,26 +33,44 @@ public class ColorHistogram {
         }
     }
 
-    public ColorHistogram(int[] bins, int cells, File img) {
-        this(bins,cells);
+    public ColorHistogram(int[] bincounts, int cellcount, File img) {
+        this(bincounts,cellcount);
         setImage(img);
     }
 
     public boolean setImage(File img){
+        empty();
         try {
-            buffered = ImageIO.read(img);
+            source = ImageIO.read(img);
+            System.out.println("Compute Histogram for " +  img.getAbsolutePath());
+            split(); //update cell array
             int rgb;
-            for (int x = 0; x<buffered.getWidth(); x++){
-                for (int y= 0; y<buffered.getHeight(); y++){
-                    rgb = buffered.getRGB(x,y);
-                    lookup[(rgb & 0x00ff0000) >> 16][(rgb & 0x0000ff00) >> 8][rgb & 0x000000ff]+=1;
-                    //System.out.println(lookup[(rgb & 0x00ff0000) >> 16][(rgb & 0x0000ff00) >> 8][rgb & 0x000000ff]);
+            int c = 1;
+            for (BufferedImage cell: cells){
+                System.out.println("Cell: " + c);
+                for (int x = 0; x<cell.getWidth(); x++){
+                    for (int y= 0; y<cell.getHeight(); y++){
+                        rgb = cell.getRGB(x,y);
+                        lookup[(rgb & 0x00ff0000) >> 16][(rgb & 0x0000ff00) >> 8][rgb & 0x000000ff]+=1;
+                    }
                 }
+                c++;
             }
         } catch (IOException e) {
             return false;
         }
         return true;
+    }
+
+    private void split(){
+        cells = new BufferedImage[cellcount*cellcount];
+        int cellwidth = source.getWidth()/cellcount;
+        int cellheight = source.getHeight()/cellcount;
+        for (int x = 0; x<cellcount; x++ ){
+            for (int y= 0; y<cellcount; y++){
+                cells[x*cellcount+y] = source.getSubimage(x*cellwidth, y*cellheight, cellwidth, cellheight);
+            }
+        }
     }
 
 }
--- src/Initiator.java
+++ src/Initiator.java
@@ -7,7 +7,11 @@ public class Initiator {
 
 
     public static void main(String[] args){
-        ColorHistogram c = new ColorHistogram(new int[]{256,256,256}, 1);
+        ColorHistogram c = new ColorHistogram(new int[]{256,256,256}, 2);
+        /*
+                {256,256,256} -> bins per color channel
+                cellcount per dimension: i.e. 4 counts in total
+         */
         File dir = new File("data/hedgehog");
         for (File img: dir.listFiles()){
            c.setImage(img);
